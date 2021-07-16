a4b2190c1267956380eaa157c8bce8a83389548a
SSA-988 <1nt19cs193.sujan@nmit.ac.in>
2021-07-16 08:54:51 +0530

    git push

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -4,6 +4,8 @@ import Row from "./Row";
 import requests from "./Requests";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
 function MainScreen() {
   const [user] = useAuthState(auth);
@@ -13,10 +15,85 @@ function MainScreen() {
         title="Action and adventure movies"
         getUrl={requests.fetchTopRated}
       />
-      <Row title={`Top Picks For ${user.displayName}`} YOU getUrl={requests.fetchActionMovies} />
+      <Row
+        title={`Top Picks For ${user.displayName}`}
+        YOU
+        getUrl={requests.fetchActionMovies}
+      />
       <Row title="COMEDY MOVIES" getUrl={requests.fetchComedyMovies} />
-      <Row title=" SUSPENSFUL HORROR MOVIES" getUrl={requests.fetchHorrorMovies} />
+      <Row
+        title=" SUSPENSFUL HORROR MOVIES"
+        getUrl={requests.fetchHorrorMovies}
+      />
       <Row title="Dark Romance" getUrl={requests.fetchRomanceMovies} />
+
+      <div className="mainscreen_loki">
+        <img
+          src="https://www.briflynews.com/sites/default/files/article/2021-05/loki%20poster_0.jpg"
+          alt=""
+        />
+        <h2>
+          Binge Watch Loki Season 1{" "}
+          <span>
+            <ChevronRightIcon className="mainscreen_icon" fontSize="large" />
+          </span>
+        </h2>
+        <div className="mainscreen_scroll">
+          <div className="mainscreen_seasons">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_1_5x/sources/r1/cms/prod/1689/1011689-h-539ca9fd6bce"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S1 E1</h2>
+          </div>
+
+          <div className="mainscreen_seasons2">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_1_5x/sources/r1/cms/prod/8386/1018386-h-63a71f18488c"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S2 E2</h2>
+          </div>
+
+          <div className="mainscreen_seasons3">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_3x/sources/r1/cms/prod/4474/1024474-h-956e10916739"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S3 E3</h2>
+          </div>
+
+          <div className="mainscreen_seasons4">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_3x/sources/r1/cms/prod/9299/1029299-h-7c22562567b5"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S4 E4</h2>
+          </div>
+
+          <div className="mainscreen_seasons5">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_3x/sources/r1/cms/prod/2220/1032220-h-9bc60e5781b0"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S5 E5</h2>
+          </div>
+
+          <div className="mainscreen_seasons6">
+            <img
+              src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_1_5x/sources/r1/cms/prod/4349/1034349-h-a917ed22e110"
+              alt=""
+            />
+            <PlayArrowIcon className="mainscreen_playIcon" />
+            <h2>S6 E6</h2>
+          </div>
+        </div>
+      </div>
       <Row title="DOCUMENTRIES" getUrl={requests.fetchDocumentaries} />
       <Row title="ANIMATED MOVIES" getUrl={requests.fetchAnimation} />
       <Row title="SCI-FI MOVIES" getUrl={requests.fetchScifi} />
